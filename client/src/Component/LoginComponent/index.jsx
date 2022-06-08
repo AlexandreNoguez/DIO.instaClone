a6f78a4968e7e1fe48fbcb270259569901b19a29
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2022-06-08 11:20:53 -0300

    input changes

--- client/src/Component/LoginComponent/index.jsx
+++ client/src/Component/LoginComponent/index.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import instaImg from '../../assets/img/insta.png'
 import { Link } from 'react-router-dom'
 import { FaFacebookSquare } from 'react-icons/fa'
 import googlePlay from '../../assets/img/android.png'
 import iphoneStore from '../../assets/img/ios.png'
 function Login() {
+    const [getEmail, setGetEmail] = useState('')
+    const [getPassword, setGetPassword] = useState('')
+    const handleLogin = e => {
+        e.preventDefault()
+        getEmail
+        getPassword
+        console.log(getEmail, getPassword)
+    }
+
     return (
         <div className="mr-36 md:mr-0 md:mt-[35.9px]">
-            <div className="w-[350px] h-[390px] p-8 border-[1px] rounded-sm bg-white items-center flex flex-col">
+            <div className="w-[350px] h-[390px] p-8 border-[1px] rounded-sm bg-white items-center">
                 <img className="mx-auto my-4" src={instaImg} />
-                <input
-                    className="w-64 h-9 p-4 border-[1px] border-gray-300 focus:outline-slate-200 rounded-sm mb-2"
-                    type="email"
-                    placeholder="Email"
-                />
-                <input
-                    className="w-64 h-8 p-4 border-[1px] border-gray-300 focus:outline-slate-200 rounded-sm mb-2"
-                    type="password"
-                    placeholder="Senha"
-                />
-                <button
-                    disabled
-                    className="w-64 p-2 rounded-md bg-blue-600 text-white hover:-translate-y-0.5 transition-all my-6"
-                >
-                    Iniciar sessão
-                </button>
+                <form className="flex flex-col justify-center items-center">
+                    <input
+                        onChange={e => setGetEmail(e.target.value)}
+                        className="w-64 h-9 p-4 border-[1px] border-gray-300 focus:outline-slate-200 rounded-sm mb-2"
+                        type="email"
+                        placeholder="Email"
+                    />
+                    <input
+                        onChange={e => setGetPassword(e.target.value)}
+                        className="w-64 h-8 p-4 border-[1px] border-gray-300 focus:outline-slate-200 rounded-sm mb-2"
+                        type="password"
+                        placeholder="Senha"
+                    />
+                    <button
+                        onClick={handleLogin}
+                        className="w-64 p-2 rounded-md bg-blue-600 text-white hover:-translate-y-0.5 transition-all my-6"
+                    >
+                        Iniciar sessão
+                    </button>
+                </form>
                 <div className="flex items-center justify-between">
                     <hr className="w-24"></hr>
                     <p className="mx-2">OU</p>
@@ -35,7 +48,7 @@ function Login() {
                     <p className="mx-2 my-2 ">Iniciar sessão com Facebook</p>
                 </div>
                 <div>
-                    <p className="text-sm my-2">
+                    <p className="text-sm my-2 text-center">
                         <Link to="/relembre">Esqueci minha senha!</Link>
                     </p>
                 </div>
@@ -53,8 +66,26 @@ function Login() {
                 <p>Baixe o app agora mesmo!</p>
             </div>
             <div className="w-[350px] my-2 items-center justify-center flex gap-2 ">
-                <img className="w-36 h-10" src={googlePlay} alt="" />
-                <img className="w-36 h-10" src={iphoneStore} alt="" />
+                <a
+                    target="_blank"
+                    href="https://play.google.com/store/apps/details?id=com.instagram.android&hl=pt_BR&gl=US"
+                >
+                    <img
+                        className="w-36 h-10"
+                        src={googlePlay}
+                        alt="ícone do link para baixar instragram na playstore"
+                    />
+                </a>
+                <a
+                    target="_blank"
+                    href="https://apps.apple.com/us/app/instagram/id389801252"
+                >
+                    <img
+                        className="w-36 h-10"
+                        src={iphoneStore}
+                        alt="ícone do link para baixar instragram na apple store"
+                    />
+                </a>
             </div>
         </div>
     )
